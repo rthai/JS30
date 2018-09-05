5265f22fc9ab17b5b5e76307e14b23760b8ec5d4
rthai <ahnamiko@gmail.com>
2018-09-04 18:52:39 -0700

    add scroll event listener, check when to slide in imgs

--- 13 - Slide in on Scroll/scripts.js
+++ 13 - Slide in on Scroll/scripts.js
@@ -12,3 +12,30 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+const images = document.querySelectorAll('.slide-in');
+
+var checkSlide = function() {
+  // go through each image
+  // find out when the window is scrolled halfway through an image
+  // find out when the window is scrolled to the bottom of an image
+  // add active class when halfway through image
+  images.forEach(image => {
+    // scrollY = position from top of window
+    // pixel level at the bottom of the window    
+    const halfway = (window.scrollY + window.innerHeight) - image.height / 2;
+    // offsetTop = distance from top of image to top of window
+    const imgBottom = image.offsetTop + image.height;
+    const isHalfway = halfway > image.offsetTop;
+    const isNotScrolledPast = window.scrollY < imgBottom;
+
+    if (isHalfway && isNotScrolledPast) {
+      image.classList.add('active');
+    } else {
+      image.classList.remove('active');
+    }
+
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
